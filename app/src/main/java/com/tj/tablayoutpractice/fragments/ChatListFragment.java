7bdcacb017bf3f98b2246e3707f6ece828f07192
Chae <chaeakf12@gmail.com>
2019-05-18 11:30:09 +0900

    프래그먼트 3개로 늘림

---
+++ app/src/main/java/com/tj/tablayoutpractice/fragments/ChatListFragment.java
@@ -0,0 +1,34 @@
+package com.tj.tablayoutpractice.fragments;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tj.tablayoutpractice.R;
+import com.tj.tablayoutpractice.databinding.FragmentChatListBinding;
+import com.tj.tablayoutpractice.databinding.FragmentHomeBinding;
+
+public class ChatListFragment extends Fragment {
+
+    FragmentChatListBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       binding = DataBindingUtil.inflate(inflater, R.layout.fragment_chat_list, container, false);
+       return binding.getRoot();
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        binding.welcomMsgTxt.setText("채팅 리스트 뷰!");
+    }
+}
